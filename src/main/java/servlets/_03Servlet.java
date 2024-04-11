318b4dafb73bafb64ede72982d985448ce695495
BIT <BIT@DESKTOP-V9BD38C>
2024-04-11 14:19:05 +0900

    jstl/el finish

--- src/main/java/servlets/_03Servlet.java
+++ src/main/java/servlets/_03Servlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +13,29 @@ public class _03Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<UserVo> list = new ArrayList<>();
+		
+		UserVo vo = null;
+		
+		vo = new UserVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		list.add(vo);
+		
+		vo = new UserVo();
+		vo.setNo(2L);
+		vo.setName("마이콜");
+		list.add(vo);
+		
+		vo = new UserVo();
+		vo.setNo(3L);
+		vo.setName("또치");
+		list.add(vo);
+		
+		request.setAttribute("list", list);
+		
 		request
-			.getRequestDispatcher("/WEB-INF/view/03.jsp")
+			.getRequestDispatcher("/WEB-INF/views/03.jsp")
 			.forward(request, response);
 	}
 
--- src/main/webapp/WEB-INF/views/03.jsp
+++ src/main/webapp/WEB-INF/views/03.jsp
@@ -1,5 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>  
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,14 @@
 <title>Insert title here</title>
 </head>
 <body>
-	
+	<h4>JSTL: forEach1</h4>
+	<c:forEach items="${list }" var="vo" varStatus="status">
+		<h6>[${status.index}:${status.count} ] ${vo.no } : ${vo.name }</h6>
+	</c:forEach>
+
+	<h4>JSTL: forEach2</h4>
+	<c:forEach begin="0" end="9" step="1" var="i">
+		<h6>${i }</h6>
+	</c:forEach>
 </body>
 </html>
